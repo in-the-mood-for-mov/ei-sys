a00cca905613cf48a49cfbefe3aeefc2b274bba4
Simon Génier <1782845+animalsiknow@users.noreply.github.com>
2019-02-13 20:16:25 -0500

    Format build.rs.

--- build.rs
+++ build.rs
@@ -7,19 +7,19 @@ const NAME: &'static str = "ei";
 const NAME: &'static str = "ei_md";
 
 fn main() {
-    let output = process::Command::new("erl")
-        .arg("-noinput")
-        .arg("-eval")
-        .arg(r#"io:format("~s", [code:root_dir()]), init:stop()."#)
-        .output()
-        .unwrap();
-    assert!(output.status.success());
-    let code_root_dir = path::PathBuf::from(String::from_utf8(output.stdout).unwrap());
-    let library_search_path = code_root_dir.join("usr/lib");
+  let output = process::Command::new("erl")
+    .arg("-noinput")
+    .arg("-eval")
+    .arg(r#"io:format("~s", [code:root_dir()]), init:stop()."#)
+    .output()
+    .unwrap();
+  assert!(output.status.success());
+  let code_root_dir = path::PathBuf::from(String::from_utf8(output.stdout).unwrap());
+  let library_search_path = code_root_dir.join("usr/lib");
 
-    println!("cargo:rustc-link-lib=static={}", NAME);
-    println!(
-        "cargo:rustc-link-search=native={}",
-        library_search_path.as_os_str().to_str().unwrap()
-    );
+  println!("cargo:rustc-link-lib=static={}", NAME);
+  println!(
+    "cargo:rustc-link-search=native={}",
+    library_search_path.as_os_str().to_str().unwrap()
+  );
 }
