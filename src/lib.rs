80dc0dc3fd1b923b7d1720bcbcc36359d69e1aca
Simon Génier <1782845+animalsiknow@users.noreply.github.com>
2019-02-13 19:31:58 -0500

    Use in_addr crate.

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,8 @@
 #![allow(non_upper_case_globals)]
 
 use core::{ffi::c_void, ops};
-
+use in_addr;
 use libc::{c_char, c_int, c_long, c_longlong, c_short, c_uchar, c_uint, c_ulong, c_ulonglong};
-
-#[cfg(not(windows))]
-pub type in_addr = libc::in_addr;
-
-#[cfg(windows)]
-pub type in_addr = winapi::shared::inaddr::in_addr;
 
 pub const ERL_TICK: c_int = 0;
 pub const ERL_MSG: c_int = 1;
@@ -221,7 +215,7 @@ extern "C" {
     thishostname: *const c_char,
     thisalivename: *const c_char,
     thisnodename: *const c_char,
-    thisipaddr: *mut in_addr,
+    thisipaddr: *mut in_addr::in_addr,
     cookie: *const c_char,
     creation: c_short,
   ) -> c_int;
@@ -242,7 +236,7 @@ extern "C" {
   /// [The official entry for this function in the Erlang documentation.](http://erlang.org/doc/man/ei_connect.html#ei_xconnect)
   pub fn ei_xconnect(
     node: *mut ei_cnode,
-    remote_host: *mut in_addr,
+    remote_host: *mut in_addr::in_addr,
     remote_name: *mut c_char,
   ) -> c_int;
 
@@ -270,7 +264,7 @@ extern "C" {
   /// [The official entry for this function in the Erlang documentation.](http://erlang.org/doc/man/ei_connect.html#ei_xconnect_tmo)
   pub fn ei_xconnect_tmo(
     node: *mut ei_cnode,
-    remote_host: *mut in_addr,
+    remote_host: *mut in_addr::in_addr,
     remote_name: *mut c_char,
     ms: c_uint,
   ) -> c_int;
